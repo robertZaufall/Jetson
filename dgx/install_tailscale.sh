f13ace5d5e4b3d440e5af510b34546fbef8d4cd5
Rob <mail@zaufall.de>
2025-12-08 23:55:12 +0100

    tailscale script added

---
+++ dgx/install_tailscale.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
+curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | tee /etc/apt/sources.list.d/tailscale.list
+
+apt-get update -y
+apt-get install tailscale -y
